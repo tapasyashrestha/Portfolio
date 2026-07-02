b653c8bf85408ff0edb1c76bd577ad39bd684b8e
tapasyashrestha <tapasyashrestha9@gmail.com>
2026-07-03 01:05:58 +0530

    style: shift camera viewport down to focus on card and prevent bottom cropping

--- src/components/BandCard.tsx
+++ src/components/BandCard.tsx
@@ -74,8 +74,8 @@ export default function BandCard() {
           dpr={[1, 2]}
           camera={{
             position: isMobile
-              ? [0, 0, 8.5]
-              : [0, 0, 6.6],
+              ? [0, -0.6, 7.5]
+              : [0, -0.95, 5.5],
             fov: isMobile ? 32 : 25,
           }}
           style={{
